e25eb83bbf61520d2e5fe8419f20f6f1d31845bb
Diya <diya@Diyas-MacBook-Air.local>
2025-11-06 14:50:55 +0530

    second

--- src/main/java/com/example/demo/DeveloperTab.java
+++ src/main/java/com/example/demo/DeveloperTab.java
@@ -30,31 +30,14 @@ class DeveloperTab {
         Label nameLabel1 = new Label("Diya Lunawat");
         nameLabel1.getStyleClass().add("developer-info");
         nameLabel1.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
-        
-        Label rollLabel1 = new Label("Roll Number: A-015");
-        rollLabel1.getStyleClass().add("developer-info");
-        
-        // Developer 2
-        Label nameLabel2 = new Label("Aishwarya Singh");
-        nameLabel2.getStyleClass().add("developer-info");
-        nameLabel2.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
-        
-        Label rollLabel2 = new Label("Roll Number: A-004");
-        rollLabel2.getStyleClass().add("developer-info");
-        
-        // Developer 3
-        Label nameLabel3 = new Label("Maahika Elenjikal");
-        nameLabel3.getStyleClass().add("developer-info");
-        nameLabel3.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
-        
-        Label rollLabel3 = new Label("Roll Number: A-029");
-        rollLabel3.getStyleClass().add("developer-info");
+
+
         
         Label descLabel = new Label("Simple, Beautiful Expense Management System");
         descLabel.getStyleClass().add("developer-info");
         descLabel.setStyle("-fx-font-size: 14px; -fx-opacity: 0.9;");
-        
-        infoCard.getChildren().addAll(nameLabel1, rollLabel1, nameLabel2, rollLabel2, nameLabel3, rollLabel3, descLabel);
+
+        infoCard.getChildren().addAll(nameLabel1,descLabel);
         container.getChildren().addAll(title, subtitle, infoCard);
         
         tab.setContent(container);
